3c3df3ca64cf680a35967aee92f1ae57bd59c585
mati-nvidia <mcodesal@nvidia.com>
2022-07-19 15:11:21 -0400

    Full functionality for CheckBoxGroup

--- exts/mc.widgets/mc/widgets/_widgets.py
+++ exts/mc.widgets/mc/widgets/_widgets.py
@@ -1,42 +1,70 @@
-import carb
+from functools import partial
 from typing import List
+
+import carb
 import omni.ui as ui
 from .styles import checkbox_group_style
 
+class CheckBoxGroupModel:
+    def __init__(self, option_names:List):
+        self.options = []
+        self.bool_models = []
+        self.subscriptions = []
+        self._single_callbacks = []
+        self._group_callbacks = []
+        for option in option_names:
+            self.add_checkbox_option(option)
+    
+    def add_checkbox_option(self, option_name):
+        self.options.append(option_name)
+        bool_model = ui.SimpleBoolModel()
+        next_index = len(self.bool_models)
+        self.bool_models.append(bool_model)
+        self.subscriptions.append(bool_model.subscribe_value_changed_fn(partial(self.on_model_value_changed, next_index)))
+        return bool_model
+    
+    def subscribe_value_changed_fn(self, callback_fn):
+        self._single_callbacks.append(callback_fn)
+    
+    def subscribe_group_changed_fn(self, callback_fn):
+        self._group_callbacks.append(callback_fn)
+
+    def on_model_value_changed(self, index:int, model:ui.SimpleBoolModel):
+        for callback in self._single_callbacks:
+            option = self.options[index]
+            callback(option, model.as_bool)
+        
+        for callback in self._group_callbacks:
+            checkbox_values = []
+            for name, bool_model in zip(self.options, self.bool_models):
+                checkbox_values.append((name, bool_model.as_bool))
+            callback(checkbox_values)
+    
+    def get_bool_model(self, option_name):
+        index = self.options.index(option_name)
+        return self.bool_models[index]
+
+    def get_checkbox_options(self):
+        return self.options
+
+    def destroy(self):
+        self.subscriptions = None
+        self._single_callbacks = None
+        self._group_callbacks = None
+
 class CheckBoxGroup:
-    def __init__(self, group_name:str, checkbox_options:List[str]):
+    def __init__(self, group_name:str, model:CheckBoxGroupModel):
         self.group_name = group_name
-        self.checkbox_options = checkbox_options
-        self.cb_group_model = CheckBoxGroupModel()
+        self.model = model
         self._build_widget()
 
     def _build_widget(self):
         with ui.VStack(width=0, height=0, style=checkbox_group_style):
             ui.Label(f"{self.group_name}:")
-            for item in self.checkbox_options:
+            for option in self.model.get_checkbox_options():
                 with ui.HStack(name="checkbox_row", width=0, height=0):
-                    model = self.cb_group_model.add_bool_model()
-                    ui.CheckBox(model=model)
-                    ui.Label(item, name="cb_label")
+                    ui.CheckBox(model=self.model.get_bool_model(option))
+                    ui.Label(option, name="cb_label")
     
     def destroy(self):
-        self.cb_group_model.destroy()
-
-class CheckBoxGroupModel:
-    def __init__(self):
-        self.sub_models = []
-        self.subscriptions = []
-    
-    def add_bool_model(self):
-        model = ui.SimpleBoolModel()
-        self.sub_models.append(model)
-        self.subscriptions.append(model.subscribe_value_changed_fn(self.on_model_value_changed))
-        return model
-
-    def on_model_value_changed(self, model:ui.SimpleBoolModel):
-        for i, model in enumerate(self.sub_models):
-            carb.log_info(f"Checkbox[{i}] is: {model.as_bool}")
-    
-    def destroy(self):
-        self.subscriptions = None
-    
+        self.model.destroy()
--- exts/mc.widgets/mc/widgets/demo.py
+++ exts/mc.widgets/mc/widgets/demo.py
@@ -1,13 +1,24 @@
-
+import carb
 import omni.ui as ui
-from ._widgets import CheckBoxGroup
+from ._widgets import CheckBoxGroup, CheckBoxGroupModel
 
 class DemoWindow(ui.Window):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         with self.frame:
             with ui.VStack():
-                self.cb_group = CheckBoxGroup("My CheckBox Group", ["Red", "Blue", "Green"])
+                model = CheckBoxGroupModel(["Red", "Blue", "Green"])
+                self.cb_group = CheckBoxGroup("My CheckBox Group", model)
+                def checkbox_changed(option_name, value):
+                    carb.log_info("This checkbox changed.")
+                    carb.log_info(f"{option_name} is {value}")
+
+                def checkbox_group_changed(values):
+                    carb.log_info("The state of my CheckBoxGroup is now:")
+                    for name, value in values:
+                        carb.log_info(f"{name} is {value}")
+                model.subscribe_value_changed_fn(checkbox_changed)
+                model.subscribe_group_changed_fn(checkbox_group_changed)
     
     def destroy(self) -> None:
         super().destroy()
